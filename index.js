df7b03f394ffd6494570b6a33ba6f40665731885
Ola Ouso <ouso.ola@gmail.com>
2020-10-24 13:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ function testFunc() {
   return "hi"
 }
 
-const result = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-
-]
-
-function superbowlWin(result) {
-  
- result.find( w => year === "W" ) 
-}
+const superbowlWin = (arr) => {
+  const result = arr.find((obj) => obj.result === "W");
+  return result ? result.year : undefined;
+};
