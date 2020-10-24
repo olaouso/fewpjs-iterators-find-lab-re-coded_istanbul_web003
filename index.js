67d0a0e195163e49d24e5a9d433a263e7a19f703
Ola Ouso <ouso.ola@gmail.com>
2020-10-24 13:42:52 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arr) => {
+  const result = arr.find((obj) => obj.result === "W");
+  return result ? result.year : undefined;
+};
